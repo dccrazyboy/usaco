aaab50b204e0752c530b72e410099b3533ed4e53
dccrazyboy <dccrazyboy@gmail.com>
2013-02-13 14:34:12 +0800

    ride

--- Chapter-1/Section-1.1/ride.cpp
+++ Chapter-1/Section-1.1/ride.cpp
@@ -9,11 +9,22 @@ LANG: C++
 
 using namespace std;
 
-int main() {
-    ofstream fout ("test.out");
-    ifstream fin ("test.in");
-    int a, b;
-    fin >> a >> b;
-    fout << a+b << endl;
+int main() 
+{
+    ofstream fout ("ride.out");
+    ifstream fin ("ride.in");
+    string comet, team;
+    fin >> comet >> team;
+    int comet_num = 1, team_num = 1;
+    int i = 0;
+    while (i < comet.length())
+        comet_num *= comet[i++]- 'A' + 1;
+    i = 0;
+    while (i < team.length())
+        team_num *= team[i++]- 'A' + 1;
+    if (comet_num%47 == team_num%47)
+        fout << "GO" << endl;
+    else
+        fout << "STAY" << endl;
     return 0;
 }
